3b96b6599667370fd150c3624ba0b2b33484135e
ohengelm <ohengelm@student.codam.nl>
2025-08-14 21:20:17 +0200

    [EDIT] refactored and removed old and commented code

--- Install.sh
+++ Install.sh
@@ -33,33 +33,12 @@ CopyFile()
 	fi
 }
 
-# RunFile()
-# {
-# 	FILE="$1"
-# 	ssh -p $PORT $USER@$ADDRESS "chmod +x $FILE; $FILE;"
-# }
-
 RunCmd()
 {
 	Command="$1";
 	# Dest="$2"
 	ssh -p $PORT $USER@$ADDRESS -t "$Command"
 }
-
-# CopyAndRun()
-# {
-# 	FILE="$1"
-# 	DEST="$2"
-
-# 	CopyFile "$FILE" "$DEST"
-# 	RunFile "$DEST/$FILE"
-# }
-
-
-# InstallPackage()
-# {
-# 	echo "Installing $1";
-# }
 
 CopyForArchLinux()
 {
@@ -71,6 +50,24 @@ CopyForLFS()
 {
 	CopyFile 	"Utils/*.sh InstallLFS/*.sh eval/*.sh" \
 				"/root";
+}
+
+CopyBLFS()
+{
+	scp -P $PORT ./ConfigureBLFS/*.sh ./Utils/*.sh $USER@$ADDRESS:/root;
+}
+
+CopyShman()
+{
+	cp ./Utils/Utils.sh ./shman/Utils.sh;
+	chmod +x ./shman/shman.sh
+	scp -P $PORT -r ./shman $USER@$ADDRESS:/usr/src/;
+	rm ./shman/Utils.sh;
+}
+
+CopyEval()
+{
+	scp -P $PORT ./eval/*.sh $USER@$ADDRESS:/eval
 }
 
 PORT=2222
@@ -87,17 +84,25 @@ while true; do
 	printf '%*s\n' "$Width" '' | tr ' ' '-';
 	echo	"Install LFS through SSH";
 	printf '%*s\n' "$Width" '' | tr ' ' '-';
-	printf	"0)\t Copy ArchLinux\n"
-	printf	"1)\t Install ArchLinux\n"
-	printf	"2)\t Copy LFS\n"
-	printf	"3)\t Install LFS\n"
+	MachineName="ft_linux"
+	printf "%-9s %s\n"	"Machine:"	"$MachineName"
+	printf "%-9s %s\n"	"Image:"	"$(VBoxManage showvminfo "$MachineName" | grep ".vdi" | awk -F'"' '{print $2}' | sed -E 's#/Snapshots/.*\.vdi#.vdi#')"
+	printf "%-9s %s\n"	"Checksum:"	"$(cat ft_linux_checksum)"
 	printf '%*s\n' "$Width" '' | tr ' ' '-';
-	printf	"4)\t Enter as Root\n"
-	printf	"5)\t Enter as arch-chroot (ArchLinux Install)\n"
-	printf	"6)\t Enter as ArchUser\n"
-	printf	"7)\t Enter as Chroot\n"
+	printf	"a)\t Install ArchLinux\n"
+	printf	"l)\t Install Linux From Scratch\n"
+	printf	"b)\t Configure Beyond Linux From Scratch\n"
+	printf	"s)\t shman - Shell package Manager\n"
+	printf	"e)\t Evaluation scripts\n"
+	printf '%*s\n' "$Width" '' | tr ' ' '-';
+	printf	"0)\t Enter as Root\n"
+	printf	"1)\t Enter as arch-chroot (ArchLinux Install)\n"
+	printf	"2)\t Enter as ArchUser\n"
+	printf	"3)\t Enter as Chroot\n"
 	printf '%*s\n' "$Width" '' | tr ' ' '-';
 	printf	"q)\t exit\n"
+	printf '%*s\n' "$Width" '' | tr ' ' '-';
+	echo	"lowercase to copy files, UPPERCASE to copy and run files"
 	printf '%*s\n' "$Width" '' | tr ' ' '-';
 	if [ ! -z "$ErrorMsg" ]; then
 		printf	"$ErrorMsg\n"
@@ -107,204 +112,29 @@ while true; do
 
 	GetKeyPress;
 	case "$input" in
-		0)	CopyForArchLinux;;
-		1)	CopyForArchLinux;
+		a)	CopyForArchLinux;;
+		A)	CopyForArchLinux;
 			RunCmd	"./InstallArchLinuxRoot.sh";;
-		2)	CopyForLFS;;
-		3)	CopyForLFS;
+		l)	CopyForLFS;;
+		L)	CopyForLFS;
 			RunCmd	"chmod +x Install*.sh; ./Install.sh";;
-		4)	ssh -p $PORT $USER@$ADDRESS || ErrorMsg="Failed to enter as Root";;
-		5)	ssh -p $PORT $USER@$ADDRESS -t "arch-chroot - ArchUser" || ErrorMsg="Failed to arch-chroot";;
-		6)	ssh -p $PORT $USER@$ADDRESS -t "su - ArchUser" || ErrorMsg="Failed to enter as ArchUser";;
-		7)	ssh -p $PORT $USER@$ADDRESS -t "chroot	\"/lfs\" /usr/bin/env -i	\
+		b|B)	CopyBLFS;;&
+			B)	ssh -p $PORT $USER@$ADDRESS -t "bash --login /root/ConfigureBLFS.sh" || ErrorMsg="$? Failed to enter as Root";;
+		s|S)	CopyShman;;&
+			S)	ssh -p $PORT $USER@$ADDRESS -t "bash --login /usr/src/shman/shman.sh" || ErrorMsg="$? Failed to enter as Root";;
+		e|E)	CopyEval;;&
+			E)	ssh -p $PORT $USER@$ADDRESS -t "cd /eval && bash --login" || ErrorMsg="$? Failed to enter as Root";;
+		0)	ssh -p $PORT $USER@$ADDRESS -t "bash --login" || ErrorMsg="$? Failed to enter as Root";;
+		1)	ssh -p $PORT $USER@$ADDRESS -t "arch-chroot - ArchUser" || ErrorMsg="$? Failed to arch-chroot";;
+		2)	ssh -p $PORT $USER@$ADDRESS -t "su - ArchUser" || ErrorMsg="$? Failed to enter as ArchUser";;
+		3)	ssh -p $PORT $USER@$ADDRESS -t "chroot	\"/lfs\" /usr/bin/env -i	\
 															HOME=/root	\
 															TERM=\"$TERM\"	\
 															PS1='(lfs chroot) \u:\w\$ '	\
 															PATH=/usr/bin:/usr/sbin	\
 															MAKEFLAGS=\"-j$(nproc)\"	\
 															TESTSUITEFLAGS=\"-j$(nproc)\"	\
-															/bin/bash --login" 	|| ErrorMsg="Failed to enter as Chroot";;
+															/bin/bash --login" 	|| ErrorMsg="$? Failed to enter as Chroot";;
 		q)	exit;;
 	esac
 done
-
-exit
-
-# PORT=2222
-# USER=root
-# ADDRESS=127.0.0.1
-# PASSWD=xxxx
-
-# COPYFILES="colors.sh default.zshrc BuildLFS.sh ConfigCrossToolchain.sh Util*.sh ConfigChroot.sh ConfigChrootPackages.sh eval/CheckBinaries.sh"
-# RUNFILES="InstallArchLinux.sh"
-
-# scp -P $PORT $COPYFILES $RUNFILES $USER@$ADDRESS:/root;
-# if [ $? -eq 1 ]; then
-# 	ssh-keygen -f "/home/ohengelm/.ssh/known_hosts" -R "[127.0.0.1]:2222";
-# 	scp -P $PORT $COPYFILES $RUNFILES $USER@$ADDRESS:/root;
-# fi
-# # if [[ $- == *i* ]]; then
-# #     echo "Shell is interactive"
-# # else
-# #     echo "Shell is not interactive"
-# # fi
-# ssh -p $PORT $USER@$ADDRESS -t "
-# 	chmod +x $RUNFILES;
-# 	zsh -i /root/InstallArchLinux.sh
-# "
-# # ssh -p $PORT $USER@$ADDRESS "
-# # chmod +x $RUNFILES;
-# # for file in $RUNFILES; do
-# # 	/root/\$file;
-# # done"
-
-# # CopyAndRun	"InstallArchLinux.sh"	"/root"
-
-# # scp -P $PORT ./InstallArchLinux.sh $USER@$ADDRESS:/root
-# # ssh -p $PORT $USER@$ADDRESS <<EOF
-# # chmod +x /root/InstallArchLinux.sh;
-# # /root/InstallArchLinux.sh;
-# # EOF
-# exit 0
-
-# InstallPackage	"lorem"
-# ssh -p 2222 root@127.0.0.1
-# exit 0;
-# echo -e "o\n n\n p\n 1\n \n \n w\n" | fdisk /dev/sda;
-# mkfs.ext4 /dev/sda1;
-# mount /dev/sda1 /mnt;
-
-# mount /dev/sda1 /mnt
-# mkdir -p /mnt/{dev,proc,sys,run}
-# mount --bind /dev /mnt/dev
-# mount --bind /run /mnt/run
-# mount -t proc /proc /mnt/proc
-# mount -t sysfs /sys /mnt/sys
-
-# pacstrap /mnt base-devel zsh
-
-# chroot /mnt /bin/zsh
-# ln -sf /proc/self/mounts /etc/mtab
-
-# # cat /etc/mtab
-
-# # pacman -Sy inetutils
-# # # pacman -Sy base-devel
-
-# curl -O https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.63.tar.xz
-
-# tar -xvf linux-6.6.63.tar.xz
-
-
-# # # Arch Linux Installation
-# # #	https://wiki.archlinux.org/title/Installation_guide
-
-# # echo "Information:";
-# # env | grep SHELL;
-
-# # # 1.5 Set the console keyboard layout and font
-
-# # # 1.6 Verify the boot mode
-
-# # # 1.7 Connect to the internet
-# # ip link
-
-# # # 1.8 Update the system clock
-# # timedatectl;
-
-# # # 1.9 Partition the disks
-# # DISK="/dev/sda"
-# # PART="/dev/sda1"
-
-# # echo Partitioning ${DISK};
-
-# # # fdisk - manipulate disk partition table
-# # #	https://man7.org/linux/man-pages/man8/fdisk.8.html
-
-# # if [ ! command -v fdisk > /dev/null ]; then
-# # 	echo "Error: fdisk is not installed";
-# # 	exit 1;
-# # fi
-
-# # if [ ! -b ${DISK} ]; then
-# # 	echo "Error: Could not find '${DISK}'";
-# # 	exit 1;
-# # fi
-
-# # if [ ! command -v fdisk ${PART} ]; then
-# # 	echo "Creating ${PART}";
-# # 	echo -e "o\n n\n p\n 1\n \n \n w\n" | fdisk ${DISK};
-# # else
-# # 	echo "${PART} already exists";
-# # fi
-
-# # # mkfs - build a Linux filesystem
-# # #	https://man7.org/linux/man-pages/man8/mkfs.8.html
-
-# # if ! mount | grep ${PART}; then
-
-# # 	echo "Formatting ${PART} with ext4 filesystem";
-# # 	if ! mkfs.ext4 ${PART} > /dev/null; then
-# # 		echo "Error: Failed to format ${PART}";
-# # 		exit 1;
-# # 	fi
-
-# # 	# mount - mount a filesystem
-# # 	#	https://man7.org/linux/man-pages/man8/mount.8.html
-
-# # 	echo "Mounting ${PART} to /mnt";
-# # 	if ! mount ${PART} /mnt > /dev/null; then
-# # 		echo "Error: Failed to mount ${PART}";
-# # 		exit 1;
-# # 	fi
-# # fi
-
-
-
-
-
-# # # Mount the partition
-# # echo "Mounting /dev/sda1 to /mnt"
-# # if ! mount /dev/sda1 /mnt; then
-# #     echo "Error: Failed to mount /dev/sda1"
-# #     exit 1
-# # fi
-
-# # # Prepare chroot environment
-# # echo "Preparing chroot environment"
-# # pacman -Sy --noconfirm arch-install-scripts
-
-# # # Mount necessary filesystems for chroot
-# # mount -t proc /proc /mnt/proc
-# # mount --rbind /sys /mnt/sys
-# # mount --rbind /dev /mnt/dev
-# # mount --rbind /run /mnt/run
-
-# # # Enter the chroot environment
-# # echo "Entering chroot environment"
-# # arch-chroot /mnt
-
-# # # Update the system and install necessary packages
-# # echo "Installing required packages"
-# # pacman -S --noconfirm base-devel linux-headers
-
-# # # Extract and install the Linux kernel (assuming you have the tarball)
-# # cd /usr/src
-# # tar -xvf /path/to/linux-6.6.63.tar.xz
-# # cd linux-6.6.63
-
-# # # Configure the kernel (this step is manual and might need modifications based on your requirements)
-# # make menuconfig
-
-# # # Compile the kernel
-# # echo "Compiling the Linux kernel"
-# # make -j$(nproc)
-
-# # # Install the kernel
-# # make modules_install
-# # make install
-
-# # # Exit chroot and unmount
-# # exit
-# # umount -R /mnt
-# # echo "Linux kernel installation complete"
